f3dc286c5866b70126cc0c64ae1dbd6ffab57e79
Petro Krasnomovets <zhyrok@gmail.com>
2020-05-03 10:08:25 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoCount, setTodoCount] = useState(0);
+  const [todo, setTodo] = useState('');
+
+  function handleDelete(id) {
+    const newTodos = todos.filter(v => v.id !== id);
+    setTodos(newTodos);
+  }
+
+  function handleCheck(id) {
+    const newTodos = [...todos];
+    let currTodo = newTodos.find(v => v.id === id);
+    currTodo.done = !currTodo.done;
+    setTodos(newTodos);
+  }
+
+  function handleAdd() {
+    setTodoCount(todoCount + 1);
+    setTodos([...todos, {id: todoCount, value: todo, done: false}])
+    setTodo('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {todos.map((todo) => (
+            <div key={todo.id}>
+              {todo.value}
+              <input type='checkbox' checked={todo.done} onChange={() => handleCheck(todo.id)}/>
+              {todo.done ? 'Completed' : 'Not Completed'}
+              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            </div>
+        ))}
+        <input type='text' value={todo} onChange={e => setTodo(e.target.value)}/>
+        <button onClick={() => handleAdd()}>Add ToDo</button>
+      </div>
   );
 }
 
